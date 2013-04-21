883ed89d1c50d920c4e3d49f79c7e522914679ea
Agis Anastasopoulos <corestudiosinc@gmail.com>
2013-04-21 16:43:08 +0300

    ignore binaries

--- ircerl.erl
+++ ircerl.erl
@@ -1,28 +1,35 @@
 -module(ircerl).
 -compile(export_all).
 
-start() ->
-  spawn(?MODULE, connect, ["127.0.0.1", 6667]).
+start(Host, Port, Nick) ->
+  spawn(?MODULE, connect, [Host, Port, Nick]).
 
-connect(Host, Port) ->
+connect(Host, Port, Nick) ->
   {ok, Socket} = gen_tcp:connect(Host, Port, [{keepalive, true}]),
-  register(Socket),
+  do(Socket, string:join(["NICK", Nick, "\r\n"], " ")),
+  do(Socket, "USER justabot 0 * :justabot\r\n"),
+  do(Socket, "JOIN #hi\r\n"),
   loop(Socket).
-
-register(Socket) ->
-  do(Socket, "NICK y0bot\r\nUSER yobot 0 * :justabot\r\n"),
-  do(Socket, "JOIN #hi\r\n").
 
 loop(Socket) ->
   receive
+    {tcp, _, "PING :irc.exampl2e.net\r\n"} ->
+      io:format("~p~n", ["ping received"]),
+      do(Socket, "PONG yothere irc.exampl2e.net"),
+      loop(Socket);
     {tcp, _, X} ->
       io:format("~p~n", [X]),
-      do(Socket, "PRIVMSG #hi :received!\r\n"),
+      do(Socket, "PRIVMSG #hi :received!"),
       loop(Socket);
     {cmd, quit} ->
-      do(Socket, "QUIT\r\n"),
-      gen_tcp:close(Socket)
+      do(Socket, "QUIT"),
+      gen_tcp:close(Socket);
+    {cmd, Command} ->
+      do(Socket, Command),
+      loop(Socket)
   end.
 
 do(Socket, Command) ->
-  gen_tcp:send(Socket, Command).
+  gen_tcp:send(Socket, string:join([Command, "\r\n"], "")).
+
+% cmd(P, Cmd) -> P ! {cmd, Cmd}.
