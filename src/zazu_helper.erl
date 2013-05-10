c884c3241bb3230e44d615e6fabae1eed19896fd
Agis Anastasopoulos <corestudiosinc@gmail.com>
2013-05-11 00:41:18 +0300

    Rename function parameter

--- src/zazu_helper.erl
+++ src/zazu_helper.erl
@@ -5,9 +5,9 @@ fetch_nick(User) ->
   string:sub_string(string:sub_word(User, 1, $!), 2).
 
 % Accepts an incoming message as a list and strips trailing newlines
-strip_msg(Message) ->
+strip_msg(L) ->
   Strip = fun(X) -> re:replace(X, "\r\n", "", [{return, list}]) end,
-  lists:map(Strip, Message).
+  lists:map(Strip, L).
 
 % Joins the elements of a list into a string with spaces
 construct_message(L) ->
